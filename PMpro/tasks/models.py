3a030530cd4554a7459e1ddcb7366b91d8911018
Dennis Ast <astasitsev@gmail.com>
2020-03-19 09:40:14 +0200

    Initital project setup + models created + users/tasks apps are set + home page

---
+++ PMpro/tasks/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from users.models import Manager
+
+status_choices = [
+    ('N/A', 'Unassigned'),
+    ('In progress', 'In progress'),
+    ('Completed', 'Completed'),
+]
+
+
+class Project(models.Model):
+    manager = models.ForeignKey(Manager, on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, choices=status_choices, default="Unassigned")
+
+    def __str__(self):
+        return "{} - {}".format(self.title, self.description)
+
+
+class Task(models.Model):
+    priority_choices = [
+        ('Low', 'Low'),
+        ('Middle', 'Middle'),
+        ('High', 'High'),
+        ('URGENT', 'URGENT'),
+    ]
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=100, choices=status_choices, default="Unassigned")
+    priority = models.CharField(max_length=50, choices=priority_choices, default="Low")
+
+    def __str__(self):
+        return "{} {}".format(self.title, self.description)
---
+++ PMpro/users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=60)
+    company_name = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        if self.company_name:
+            return "{} {} - ({})".format(self.name, self.last_name, self.company_name)
+        return "{} {}".format(self.name, self.last_name)
