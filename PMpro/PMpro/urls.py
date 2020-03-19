2c5ab264bb17a06b73ccbd0c2129c48e64311462
Dennis Ast <astasitsev@gmail.com>
2020-03-19 10:53:54 +0200

    updated urls.py, admin.py

--- PMpro/PMpro/urls.py
+++ PMpro/PMpro/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', TemplateView.as_view(template_name='index.html'))
+    path('index/', TemplateView.as_view(template_name='index.html'))
 ]
 
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- PMpro/tasks/admin.py
+++ PMpro/tasks/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Manager, Project, Task
+from .models import Project, Task
 
 admin.site.register(Project)
 admin.site.register(Task)
