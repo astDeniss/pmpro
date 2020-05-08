c0dd2c6b4419cc5c19ceae363c48c74bb9ad9e34
loudmouth696 <edv.zagorski@gmail.com>
2020-05-08 21:39:06 +0300

    registration

---
+++ PMpro/users/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
+    company_name = forms.CharField(max_length=50)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name','company_name', 'password1', 'password2', )
+
+    def __str__(self):
+        return "{} {}".format(self.username, self.company_name)
+
