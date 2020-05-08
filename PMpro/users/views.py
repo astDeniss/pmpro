b6421a5dfb0e9fd19db41f1096f5652bfc93b318
loudmouth696 <edv.zagorski@gmail.com>
2020-05-08 21:36:29 +0300

    registration

--- PMpro/users/views.py
+++ PMpro/users/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+from .models import Manager
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+
+            user_profile = User.objects.create(username=username, password=password, first_name=first_name, last_name=last_name)
+            #user_profile.save()
+            
+
+            our_user = User.objects.get(username=username)
+            
+            company_name = form.cleaned_data.get('company_name')
+            manager_profile = Manager.objects.create(user=our_user, company_name=company_name)
+            #manager_profile.save()
+            
+
+            #user = authenticate(username=username, password=password)
+            #login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration_page.html', {'form': form})
+
