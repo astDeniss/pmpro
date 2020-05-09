2ff39f0e022d1513cb2de22bedbf0a7e2a8591f5
Dennis Ast <astasitsev@gmail.com>
2020-05-09 11:52:03 +0300

    login finished

--- PMpro/tasks/urls.py
+++ PMpro/tasks/urls.py
@@ -1,13 +1,13 @@
-
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import ProjectListView, add_project, ProjectDetailView, ProjectDeleteView, TaskListView
 
 urlpatterns = [
 
-    path('projects/', ProjectListView.as_view(), name="projects-list"),
-    path('projects/add/', add_project, name="add-project"),
-    path('projects/<int:pk>/', ProjectDetailView.as_view(), name="project-detail"),
-    path('projects/delete/<int:pk>/', ProjectDeleteView.as_view(), name="project-delete"),
-    path('tasks/', TaskListView.as_view(), name="tasks-list"),
+    path('projects/', login_required(ProjectListView.as_view()), name="projects-list"),
+    path('projects/add/', login_required(add_project), name="add-project"),
+    path('projects/<int:pk>/', login_required(ProjectDetailView.as_view()), name="project-detail"),
+    path('projects/delete/<int:pk>/', login_required(ProjectDeleteView.as_view()), name="project-delete"),
+    path('tasks/', login_required(TaskListView.as_view()), name="tasks-list"),
 
 ]
--- PMpro/users/views.py
+++ PMpro/users/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
@@ -7,7 +9,6 @@ from django.contrib.auth.models import User
 
 
 def signup_view(request):
-
     if request.user.is_authenticated:
         return redirect('index')
 
@@ -25,7 +26,7 @@ def signup_view(request):
             company_name = form.cleaned_data.get('company_name')
             manager_profile = Manager(user=our_user, company_name=company_name)
             manager_profile.save()
-            
+
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('index')
@@ -33,3 +34,25 @@ def signup_view(request):
         form = SignUpForm()
     return render(request, 'registration_page.html', {'form': form})
 
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return HttpResponse("You are already logged in :)")
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                if 'next' in request.POST:
+                    return redirect(request.POST.get('next'))
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your account is inactive.")
+        else:
+
+            return HttpResponse("Invalid login details given. Go back and try again")
+    else:
+        return render(request, 'login.html', {})
